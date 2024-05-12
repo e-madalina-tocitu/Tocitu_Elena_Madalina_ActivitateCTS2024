53c606bdf92ac8b48ce3f8c4a8267a6787ec44a8
e-madalina-tocitu <madalinatocitu94@gmail.com>
2024-05-12 20:41:37 +0300

    Seminar

---
+++ Seminar11/src/template/main/Main.java
@@ -0,0 +1,11 @@
+package template.main;
+
+import template.clase.AchizitieMedicament;
+
+public class Main {
+    public static void main(String[] args) {
+        AchizitieMedicament achizitieMedicament = new AchizitieMedicament("Paracetamol");
+        achizitieMedicament.cumparaMedicament();
+        achizitieMedicament.cumparaMedicament();
+    }
+}
