f4a7276c4393ef21a0ab5fc34261b122892bd85a
e-madalina-tocitu <madalinatocitu94@gmail.com>
2024-05-29 12:13:55 +0300

    Seminar

---
+++ Seminar14/src/teste/suite/SuitaCompleta.java
@@ -0,0 +1,13 @@
+package teste.suite;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import teste.PachetTuristicTest;
+import teste.PachetTuristicTestWithFixture;
+import teste.PersoanaTests;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PachetTuristicTest.class, PachetTuristicTestWithFixture.class, PersoanaTests.class})
+public class SuitaCompleta {
+}
